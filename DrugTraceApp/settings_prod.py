094c5f8d06e781255a8fdc4bfbfe6871a06b90e7
Varshitha <165037791+varshitha127@users.noreply.github.com>
2025-05-23 20:37:09 +0530

    settings_prod.py

--- DrugTraceApp/settings_prod.py
+++ DrugTraceApp/settings_prod.py
@@ -21,6 +21,8 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', ''),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
