dcb5c48d93cdecd1da6da8ecd5029b9b4e65394b
varshitha127 <165037791+varshitha127@users.noreply.github.com>
2025-05-27 19:29:11 +0530

    Update settings for Render deployment

--- DrugTraceApp/settings.py
+++ DrugTraceApp/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Allow all hosts in development
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
